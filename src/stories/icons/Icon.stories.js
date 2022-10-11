eb7c9b086e693f15cd767d26acdcf666cbea0168
jeremyn-nmq <jeremyn-nmq@users.noreply.github.com>
2022-10-11 09:44:19 +0000

    Deploying to gh-pages from  @ 02115cd415244219e1d6a76b63228231ed3b9428 🚀

--- src/stories/icons/Icon.stories.js
+++
@@ -1,9 +0,0 @@
-import IconComponent from '@/components/atoms/Icon';
-
-export const IconTemplate = args => ({
-    components: { IconComponent },
-    setup() {
-        return { args };
-    },
-    template: '<IconComponent v-bind="args"/>',
-});
